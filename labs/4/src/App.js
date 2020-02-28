5250173499f130c8e51c19d3c1058077e700157f
Alyssa Barella <Alyssa@Alyssas-MacBook-Air.local>
2020-02-28 13:35:09 -0500

    lab 4

---
+++ labs/4/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import CardList from './component/CardList/index.js'
+
+class App extends React.Component {
+  render() {
+    return(
+      <CardList/>
+    )
+  }
+}
+
+
+
+export default App;
